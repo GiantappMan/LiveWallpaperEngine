67c2723c3dc5852e85c25a0fc1ad88c19dc49749
snowlab <snowlab@hotmail.com>
2018-10-19 17:33:21 +0800

    基本功能

--- LiveWallpaperEngine/LWECore.cs
+++ LiveWallpaperEngine/LWECore.cs
@@ -1,4 +1,5 @@
-﻿//https://www.codeproject.com/Articles/856020/Draw-Behind-Desktop-Icons-in-Windows
+﻿//https://www.codeproject.com/Articles/856020/Draw-Behind-Desktop-Icons-in-Windows 动态壁纸核心逻辑，就这么两句。。没什么好藏在噎着的。代码我也是从互联网抄写的-。-
+//https://github.com/Francesco149/weebp/blob/master/src/weebp.c 从时间上来说这个仓库比我后写，但是却有值得学习的地方。毕竟是c语言大佬。。。
 using DZY.WinAPI;
 using DZY.WinAPI.Desktop.API;
 using System;
@@ -6,7 +7,7 @@ using System;
 namespace LiveWallpaperEngine
 {
     /// <summary>
-    /// 动态壁纸核心逻辑，就这么两句。。没什么好藏在噎着的。代码我也是从互联网抄写的-。-
+    /// 
     /// </summary>
     public static class LWECore
     {
@@ -39,7 +40,7 @@ namespace LiveWallpaperEngine
             UnshownEvent?.Invoke(null, _targeHandler);
         }
 
-        public static bool SendToBackground(IntPtr handler, bool disableOSWallpaper = true)
+        public static bool SendToBackground(IntPtr handler, bool disableOSWallpaper = true, bool fullScreen = true)
         {
             if (handler == IntPtr.Zero || Shown)
                 return false;
@@ -57,11 +58,28 @@ namespace LiveWallpaperEngine
             _parentHandler = User32Wrapper.GetParent(_targeHandler);
             if (_parentHandler == IntPtr.Zero)
                 _parentHandler = User32Wrapper.GetAncestor(_targeHandler, GetAncestorFlags.GetParent);
+
             User32Wrapper.SetParent(_targeHandler, _workerw);
+
+            if (fullScreen)
+                FullScreen(_targeHandler);
+
             if (_disableOSWallpaper)
                 _desktopWallpaperAPI.Enable(false);
             ShownEvent?.Invoke(null, _targeHandler);
             return true;
+        }
+
+        private static void FullScreen(IntPtr targeHandler)
+        {
+            var tmp = User32Wrapper.MonitorFromWindow(targeHandler, User32Wrapper.MONITOR_DEFAULTTONEAREST);
+            MONITORINFO info = new MONITORINFO();
+
+            bool ok = User32Wrapper.GetMonitorInfo(tmp, info);
+            if (!ok)
+                return;
+
+            ok = User32Wrapper.SetWindowPos(targeHandler, IntPtr.Zero, info.rcMonitor.Left, info.rcMonitor.Top, info.rcMonitor.Width, info.rcMonitor.Height, 0);
         }
 
         private static bool Initlize()
@@ -76,7 +94,7 @@ namespace LiveWallpaperEngine
                                    1000,
                                    out result);
             _workerw = IntPtr.Zero;
-            var result1 = User32Wrapper.EnumWindows(new EnumWindowsProc((tophandle, topparamhandle) =>
+            var enumWindowResult = User32Wrapper.EnumWindows(new EnumWindowsProc((tophandle, topparamhandle) =>
             {
                 IntPtr p = User32Wrapper.FindWindowEx(tophandle,
                                             IntPtr.Zero,
@@ -93,8 +111,8 @@ namespace LiveWallpaperEngine
 
                 return true;
             }), IntPtr.Zero);
-            _initlized = result1;
-            return result1;
+            _initlized = enumWindowResult;
+            return enumWindowResult;
         }
     }
 }
