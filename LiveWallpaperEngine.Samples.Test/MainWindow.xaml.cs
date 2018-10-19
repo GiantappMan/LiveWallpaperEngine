bd554ce95fa6cbaba01f91b2cca2977b82841baa
DaZiYuan <mscoder@hotmail.com>
2018-10-19 14:58:06 +0800

    进程列表

--- LiveWallpaperEngine.Samples.Test/MainWindow.xaml.cs
+++ LiveWallpaperEngine.Samples.Test/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,5 +25,42 @@ namespace LiveWallpaperEngine.Samples.Test
         {
             InitializeComponent();
         }
+
+        int lastIndex = -1;
+        private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            TabControl tb = sender as TabControl;
+            if (lastIndex == tb.SelectedIndex)
+                return;
+            lastIndex = tb.SelectedIndex;
+            switch (lastIndex)
+            {
+                case 0:
+                    LoadProcess();
+                    break;
+                case 1: break;
+            }
+        }
+        #region tabitem1
+        private void LoadProcess()
+        {
+            var allProcesses = Process.GetProcesses().Where(m => !string.IsNullOrEmpty(m.MainWindowTitle) || m.MainWindowHandle != IntPtr.Zero).ToList();
+            lstBoxProcess.ItemsSource = allProcesses;
+        }
+
+        private void btnCloseProcess_Click(object sender, RoutedEventArgs e)
+        {
+            var btn = sender as Button;
+            LWECore.RestoreParent();
+        }
+
+        private void btnShowProcess_Click(object sender, RoutedEventArgs e)
+        {
+            var btn = sender as Button;
+            var process = btn.DataContext as Process;
+            LWECore.SendToBackground(process.MainWindowHandle, chkDisableOSWallpaper.IsChecked.Value);
+        }
+        #endregion
+
     }
 }
