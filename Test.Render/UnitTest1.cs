ddd2fed11fa9883c62098a5f84918d0c0eb830d0
DaZiYuan <mscoder@hotmail.com>
2019-03-08 18:29:34 +0800

    单元测试

---
+++ Test.Render/UnitTest1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using LiveWallpaperEngine;
+using LiveWallpaperEngineRender.Renders;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test.Render
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestVideoRender()
+        {
+            IRender render = GetVideoRender();
+            TestRender(render);
+        }
+
+        private void TestRender(IRender render)
+        {
+            Assert.AreEqual(render.Paused, false);
+            Assert.AreEqual(render.Playing, false);
+
+            render.Play("ss");
+            Assert.AreEqual(render.Playing, true);
+
+            render.Pause();
+            Assert.AreEqual(render.Playing, false);
+            Assert.AreEqual(render.Paused, true);
+
+            render.Resume();
+            Assert.AreEqual(render.Playing, true);
+            Assert.AreEqual(render.Paused, false);
+
+            render.Stop();
+            Assert.AreEqual(render.Playing, false);
+            Assert.AreEqual(render.Paused, false);
+
+            render.CloseRender();
+            Assert.AreEqual(render.Paused, false);
+            Assert.AreEqual(render.Playing, false);
+        }
+
+        private IRender GetVideoRender()
+        {
+            IRender render = new VideoRender();
+            return render;
+        }
+    }
+}
