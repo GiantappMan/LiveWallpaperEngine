deccf7f913d1a2a876f5ec8a5079408298963800
mscoder <mscoder@hotmail.com>
2019-03-12 18:31:09 +0800

    explorer 崩溃后重启

--- LiveWallpaperEngine/LiveWallpaperEngineManager.cs
+++ LiveWallpaperEngine/LiveWallpaperEngineManager.cs
@@ -3,17 +3,46 @@ using System.Collections.Generic;
 using System.Text;
 using System.Linq;
 using System.Windows.Forms;
+using System.Windows.Threading;
 
 namespace LiveWallpaperEngine
 {
+    public class LiveWallpaperEngineCoreEx : LiveWallpaperEngineCore
+    {
+        internal LiveWallpaperEngineCoreEx(Screen screen) : base(screen) { }
+        public IRender Render { get; set; }
+    }
+
+    /// <summary>
+    /// 管理多屏壁纸
+    /// </summary>
     public class LiveWallpaperEngineManager
     {
         public static List<Screen> AllScreens { get; private set; }
+        public static Dispatcher UIDispatcher { get; set; }
 
-        static List<LiveWallpaperEngineCore> cores = new List<LiveWallpaperEngineCore>();
+        static List<LiveWallpaperEngineCoreEx> cores = new List<LiveWallpaperEngineCoreEx>();
         static LiveWallpaperEngineManager()
         {
+            ExplorerMonitor.ExpolrerCreated += _explorerMonitor_ExpolrerCreated;
+            ExplorerMonitor.Start();
+
             SetupCores();
+        }
+
+        private static void _explorerMonitor_ExpolrerCreated(object sender, EventArgs e)
+        {//explorer 重启
+            cores.ForEach(m =>
+            {
+                m?.ReInit();
+                if (m.Render != null)
+                {
+                    IntPtr handle = IntPtr.Zero; ;
+                    handle = m.Render.RestartRender();
+                    m.SendToBackground(handle);
+                    m.Render.Play(m.Render.CurrentPath);
+                }
+            });
         }
 
         public static void SetupCores()
@@ -21,7 +50,7 @@ namespace LiveWallpaperEngine
             AllScreens = new List<Screen>(Screen.AllScreens);
             AllScreens.ForEach(item =>
              {
-                 cores.Add(new LiveWallpaperEngineCore(item));
+                 cores.Add(new LiveWallpaperEngineCoreEx(item));
              });
         }
 
@@ -33,11 +62,12 @@ namespace LiveWallpaperEngine
             if (core == null)
                 return false;
 
+            core.Render = render;
             bool ok = core.SendToBackground(handle);
             return ok;
         }
 
-        public static LiveWallpaperEngineCore GetCore(Screen screen)
+        public static LiveWallpaperEngineCoreEx GetCore(Screen screen)
         {
             var result = cores.FirstOrDefault(m => m.DisplayScreen == screen);
             return result;
---
+++ LiveWallpaperEngineRender/Renders/MpvForm.cs
@@ -0,0 +1,43 @@
+﻿using LiveWallpaperEngine;
+using Mpv.NET.Player;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LiveWallpaperEngineRender.Renders
+{
+    public partial class MpvForm : Form
+    {
+        public MpvPlayer Player { get; set; }
+
+        #region construct
+
+        public MpvForm()
+        {
+            //UI
+            BackColor = Color.Magenta;
+            TransparencyKey = Color.Magenta;
+            InitializeComponent();
+            FormBorderStyle = FormBorderStyle.None;
+
+            //callback
+            FormClosing += RenderForm_FormClosing;
+        }
+
+
+        private void RenderForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            FormClosing -= RenderForm_FormClosing;
+            Player?.Dispose();
+        }
+
+        #endregion
+    }
+}
