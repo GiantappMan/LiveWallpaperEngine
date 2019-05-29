003a27a0415c9b01631ef9d2e4ed2bd7e980a17d
DaZiYuan <mscoder@hotmail.com>
2019-05-29 18:34:00 +0800

    .net core 3.0

--- LiveWallpaperEngineRender/Renders/VideoRender.cs
+++ LiveWallpaperEngineRender/Renders/VideoRender.cs
@@ -7,6 +7,7 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Threading;
 
 namespace LiveWallpaperEngineRender.Renders
 {
