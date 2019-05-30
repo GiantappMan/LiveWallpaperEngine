7025e4459010771f0fb3538bdbc5e96d7a324ad7
DaZiYuan <mscoder@hotmail.com>
2019-05-30 12:55:49 +0800

    修复dpi问题

--- LiveWallpaperEngine/LiveWallpaperEngineManager.cs
+++ LiveWallpaperEngine/LiveWallpaperEngineManager.cs
@@ -51,7 +51,7 @@ namespace LiveWallpaperEngine
         }
 
         public static void SetupCores()
-        {
+        {            
             User32WrapperEx.SetThreadAwarenessContext(DPI_AWARENESS_CONTEXT.DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE);
             AllScreens = new List<Screen>(Screen.AllScreens);
             AllScreens.ForEach(item =>
