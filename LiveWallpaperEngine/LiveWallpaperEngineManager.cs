5cfa66537a77132ecc3ad8a2c1e1a07b36b8d52f
DaZiYuan <mscoder@hotmail.com>
2019-05-30 13:11:08 +0800

    调整代码位置

--- LiveWallpaperEngine/LiveWallpaperEngineManager.cs
+++ LiveWallpaperEngine/LiveWallpaperEngineManager.cs
@@ -31,34 +31,7 @@ namespace LiveWallpaperEngine
             SetupCores();
         }
 
-        private static void _explorerMonitor_ExpolrerCreated(object sender, EventArgs e)
-        {//explorer 重启
-            cores.ForEach(m =>
-            {
-                m?.ReInit();
-                if (m.Render != null)
-                {
-                    IntPtr handle = IntPtr.Zero; ;
-                    handle = m.Render.RestartRender();
-                    m.SendToBackground(handle);
-                    if (m.Render is IVideoRender)
-                    {
-                        var tmp = m.Render as IVideoRender;
-                        tmp.Play(tmp.CurrentPath);
-                    }
-                }
-            });
-        }
-
-        public static void SetupCores()
-        {            
-            User32WrapperEx.SetThreadAwarenessContext(DPI_AWARENESS_CONTEXT.DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE);
-            AllScreens = new List<Screen>(Screen.AllScreens);
-            AllScreens.ForEach(item =>
-             {
-                 cores.Add(new LiveWallpaperEngineCoreEx(item));
-             });
-        }
+        #region public methods
 
         public static bool Show(IRender render, Screen screen)
         {
@@ -95,16 +68,49 @@ namespace LiveWallpaperEngine
             core.Close();
         }
 
+        public static LiveWallpaperEngineCoreEx GetCore(Screen screen)
+        {
+            var result = cores.FirstOrDefault(m => m.DisplayScreen == screen);
+            return result;
+        }
+
+        #endregion
+
+
+        private static void _explorerMonitor_ExpolrerCreated(object sender, EventArgs e)
+        {//explorer 重启
+            cores.ForEach(m =>
+            {
+                m?.ReInit();
+                if (m.Render != null)
+                {
+                    IntPtr handle = IntPtr.Zero; ;
+                    handle = m.Render.RestartRender();
+                    m.SendToBackground(handle);
+                    if (m.Render is IVideoRender)
+                    {
+                        var tmp = m.Render as IVideoRender;
+                        tmp.Play(tmp.CurrentPath);
+                    }
+                }
+            });
+        }
+
+        private static void SetupCores()
+        {
+            User32WrapperEx.SetThreadAwarenessContext(DPI_AWARENESS_CONTEXT.DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE);
+            AllScreens = new List<Screen>(Screen.AllScreens);
+            AllScreens.ForEach(item =>
+             {
+                 cores.Add(new LiveWallpaperEngineCoreEx(item));
+             });
+        }
+
         private static LiveWallpaperEngineCoreEx GetCore(IRender render)
         {
             var result = cores.FirstOrDefault(m => m.Render == render);
             return result;
         }
 
-        public static LiveWallpaperEngineCoreEx GetCore(Screen screen)
-        {
-            var result = cores.FirstOrDefault(m => m.DisplayScreen == screen);
-            return result;
-        }
     }
 }
