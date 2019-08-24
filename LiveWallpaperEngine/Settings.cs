0150ecd4f1d93d94fdba247bfea73a2e03e05754
DaZiYuan <mscoder@hotmail.com>
2019-08-25 01:27:14 +0800

    定义新接口

---
+++ LiveWallpaperEngine/Settings.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiveWallpaperEngine
+{
+    public enum ActionWhenMaximized
+    {
+        Play,
+        Pause,
+        Stop
+    }
+    public struct ScreenSetting
+    {
+        public int ScreenIndex { get; set; }
+        /// <summary>
+        /// 当前窗口其他程序全屏时
+        /// </summary>
+        public ActionWhenMaximized WhenCurrentScreenMaximized { get; set; }
+    }
+
+    public struct Settings
+    {
+        /// <summary>
+        /// 播放音频的显示器
+        /// </summary>
+        public static int AudioScreenIndex { get; set; }
+        /// <summary>
+        /// 每个屏幕的单独设置
+        /// </summary>
+        public static ScreenSetting[] ScreenScreenSettings { get; set; }
+    }
+}
---
+++ LiveWallpaperEngine/WallpaperManager.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiveWallpaperEngine
+{
+    /// <summary>
+    /// 壁纸管理
+    /// </summary>
+    public static class WallpaperManager
+    {
+        public static Settings Settings { get; private set; }
+
+        public static Task SetSettings(Settings settings)
+        {
+            return Task.CompletedTask;
+        }
+
+        #region public methods
+
+        /// <summary>
+        /// 显示壁纸                
+        /// </summary>
+        /// <remarks>       
+        /// 视频
+        /// todo 网页
+        /// todo exe
+        /// todo 图片地址
+        /// </remarks>
+        /// <param name="filepath">壁纸路经，内部自动按后缀处理。</param>
+        /// <param name="screenIndexs">显示壁纸的屏幕索引，从0开始</param>
+        public static void ShowWallpaper(string filepath, int[] screenIndexs)
+        {
+            //todo
+        }
+
+        #endregion
+
+        #region private methods
+
+        /// <summary>
+        /// 暂停壁纸
+        /// </summary>
+        /// <param name="screendIndexs"></param>
+        private static void Pause(int[] screendIndexs)
+        {
+
+        }
+
+        /// <summary>
+        /// 恢复壁纸
+        /// </summary>
+        /// <param name="screendIndexs"></param>
+        private static void Resume(int[] screendIndexs)
+        {
+
+        }
+
+        #endregion
+
+    }
+}
