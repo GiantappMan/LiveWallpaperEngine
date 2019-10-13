fe7c667dcd63bb068b5c7c15cfba9c81e855ac3d
DaZiYuan <mscoder@hotmail.com>
2019-10-14 00:07:32 +0800

    grpc 联调

--- LiveWallpaperWebRender/src/index.js
+++ LiveWallpaperWebRender/src/index.js
@@ -5,9 +5,28 @@
 // https://electronjs.org/docs/api/browser-window
 
 const { app, BrowserWindow } = require('electron')
+const grpc = require('@grpc/grpc-js');
+const protoLoader = require('@grpc/proto-loader');
 
+let protoFile = `${__dirname}\\..\\..\\Protos\\api.proto`;
+protoLoader.load(protoFile).then(packageDefinition => {
+  const proto = grpc.loadPackageDefinition(packageDefinition);
+  // let client = new service.LiveWallpaperEngine("http://127.0.0.1:8080");
+  // const client = http2.connect("http://127.0.0.1:8080");
+
+  let package = proto["LiveWallpaperEngine"];
+  // let client = grpc.makeClientConstructor(packageDefinition.LiveWallpaperEngine, "http://127.0.0.1:8080");
+  let client = new package["API"]("127.0.0.1:8080", grpc.credentials.createInsecure());
+  // var test = client.CloseWallpaper([1, 2]);
+
+  let test = client["CloseWallpaper"]({
+    "ScreenIndexs": [3, 2]
+  }, (e1) => {
+    console.log("success" + e1);
+  });
+});
 const windows = []
-
+console.error("test");
 app.on('ready', () => {
   // BrowserWindows can be created in plenty of shapes, sizes, and forms.
   // Check out the editor's auto-completion for all the configuration
@@ -16,11 +35,16 @@ app.on('ready', () => {
   // Let's make a few windows!
 
   // A transparent window
-  var window = new BrowserWindow({
+  let window = new BrowserWindow({
     skipTaskbar: true,
     frame: false,
   });
+
+  window.openDevTools();
+
+
   let handle = window.getNativeWindowHandle().readUInt32LE();
+  window
   windows.push(window)
 
   windows.forEach((window) => {
