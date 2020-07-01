8905b4fb18105082e599120b9c3616d0d33a21e5
DaZiYuan <mscoder@hotmail.com>
2020-07-01 10:59:20 +0800

    暂停exe

--- LiveWallpaperEngineAPI/Forms/ExeRenderControl.cs
+++ LiveWallpaperEngineAPI/Forms/ExeRenderControl.cs
@@ -14,6 +14,53 @@ using DZY.WinAPI;
 
 namespace Giantapp.LiveWallpaper.Engine.Forms
 {
+    public static class ProcessExtension
+    {
+        [Flags]
+        public enum ThreadAccess : int
+        {
+            TERMINATE = (0x0001),
+            SUSPEND_RESUME = (0x0002),
+            GET_CONTEXT = (0x0008),
+            SET_CONTEXT = (0x0010),
+            SET_INFORMATION = (0x0020),
+            QUERY_INFORMATION = (0x0040),
+            SET_THREAD_TOKEN = (0x0080),
+            IMPERSONATE = (0x0100),
+            DIRECT_IMPERSONATION = (0x0200)
+        }
+        [DllImport("kernel32.dll")]
+        static extern IntPtr OpenThread(ThreadAccess dwDesiredAccess, bool bInheritHandle, uint dwThreadId);
+        [DllImport("kernel32.dll")]
+        static extern uint SuspendThread(IntPtr hThread);
+        [DllImport("kernel32.dll")]
+        static extern int ResumeThread(IntPtr hThread);
+
+        public static void Suspend(this Process process)
+        {
+            foreach (ProcessThread thread in process.Threads)
+            {
+                var pOpenThread = OpenThread(ThreadAccess.SUSPEND_RESUME, false, (uint)thread.Id);
+                if (pOpenThread == IntPtr.Zero)
+                {
+                    break;
+                }
+                SuspendThread(pOpenThread);
+            }
+        }
+        public static void Resume(this Process process)
+        {
+            foreach (ProcessThread thread in process.Threads)
+            {
+                var pOpenThread = OpenThread(ThreadAccess.SUSPEND_RESUME, false, (uint)thread.Id);
+                if (pOpenThread == IntPtr.Zero)
+                {
+                    break;
+                }
+                ResumeThread(pOpenThread);
+            }
+        }
+    }
     //显示exe的容器
     public partial class ExeRenderControl : UserControl, IRenderControl
     {
@@ -36,10 +83,14 @@ namespace Giantapp.LiveWallpaper.Engine.Forms
 
         public void Pause()
         {
+            var p = Process.GetProcessById(_currentPid);
+            p.Suspend();
         }
 
         public void Resum()
         {
+            var p = Process.GetProcessById(_currentPid);
+            p.Resume();
         }
 
         public void SetVolume(int volume)
